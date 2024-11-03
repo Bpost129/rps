f0ededa5cc0cbfc4831d72c55ee2328975a936f9
Bpost129 <Bpost129@yahoo.com>
2024-11-02 21:33:23 -0400

    add function to compare choices, render message based on results of comparison

--- js/app.js
+++ js/app.js
@@ -24,7 +24,34 @@ function getPlayerChoice(e) {
   playerChoice = e.target.id
 }
 
+function getCpuChoice() {
+  const randomIdx = Math.floor(Math.random() * choices.length)
+  cpuChoice = choices[randomIdx]
+}
+
+function compare() {
+  if (playerChoice === cpuChoice) {
+    msg = 'You tied!'
+  } else if (playerChoice === choices[0] && cpuChoice === choices[2]) {
+    msg = 'Congrats! You win!'
+  } else if (playerChoice === choices[1] && cpuChoice === choices[0]) {
+    msg = 'Congrats! You win!'
+  } else if (playerChoice === choices[2] && cpuChoice === choices[1]) {
+    msg = 'Congrats! You win!'
+  } else {
+    msg = 'You lose! Try again?'
+  }
+}
+
+function render() {
+  resultEl.textContent = `You chose ${playerChoice} and the computer chose ${cpuChoice}. ${msg}`
+}
+
 function play(e) {
   getPlayerChoice(e)
-  console.log(playerChoice)
+  getCpuChoice()
+  compare()
+  render()
+  // console.log(playerChoice)
+  // console.log(cpuChoice)
 }
